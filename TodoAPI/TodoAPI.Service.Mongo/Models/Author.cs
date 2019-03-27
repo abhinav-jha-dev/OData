e2dfde1bdec07bf66299d817948adbf49620aec7
Abhinav Jha <abhinav.jha@tm.chd>
2019-03-27 19:16:11 +0530

    Updated Models

--- TodoAPI/TodoAPI.Service.Mongo/Models/Author.cs
+++ TodoAPI/TodoAPI.Service.Mongo/Models/Author.cs
@@ -1,13 +1,15 @@
 ﻿using Todo.Service.Mongo.Entity;
-using Todo.Service.Mongo.Utilities.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Todo.Service.Mongo.Models
 {
     public class Author : IBaseEntity
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
         public string Description { get; set; }
--- TodoAPI/TodoAPI.Service.Mongo/Models/TodoTask.cs
+++ TodoAPI/TodoAPI.Service.Mongo/Models/TodoTask.cs
@@ -2,12 +2,15 @@ using Todo.Service.Mongo.Entity;
 using Todo.Service.Mongo.Utilities.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Todo.Service.Mongo.Models
 {
     public class TodoTask
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
         public IList<string> Tags { get; set; }
